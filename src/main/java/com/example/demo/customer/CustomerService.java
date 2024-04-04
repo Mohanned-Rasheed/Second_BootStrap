332a46d926595e0e915cf2019e2467c67c7f00b9
Mohanned-Rasheed <mohanned3211@gmail.com>
2024-04-04 17:48:48 +0300

    Spring Boot Actuator and Metrics

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,12 +1,15 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     //@Qualifier("fake")
@@ -15,11 +18,17 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers Was Called!");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
-        return customerRepository.findById(id).orElseThrow(() -> new NotFoundException("customer with id:" + id + " not found"));
+        LOGGER.info("getCustomer Was Called!");
+        return customerRepository.findById(id).orElseThrow(() -> {
+            NotFoundException notFoundException = new NotFoundException("customer with id:" + id + " not found");
+            LOGGER.error("error in getCustomer {}", id, notFoundException);
+            return notFoundException;
+        });
 
     }
 }
